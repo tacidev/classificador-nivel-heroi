ff3b32867ba79d989aa5746548352f07ecbfa778
tacidev <tfrioli@gmail.com>
2025-09-07 11:05:47 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 console.log ("Classificador de Nível de Herói")
 
 
-const readline = require("readline");
+const readline = require("readline-sync");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+for (let i = 1; i <= 3; i++) {
 
-rl.question("Informe seu Herói: ", (nome) => {
-  console.log ("Seu herói é " + nome)
-  rl.close();
-});
+  let nome = readline.question("Informe o nome do Herói: ");
+  let quantidadeXP = readline.question("Informe a quantidade de XP: ");
+
+  let resultado = "Seu herói é " + nome + " e está no nível ";
+
+   switch (true){
+    case (quantidadeXP <= 1000):
+      console.log (resultado + "Ferro!");
+      break;
+
+    case (quantidadeXP >= 1001 && quantidadeXP <= 2000):
+      console.log (resultado + "Bronze!");
+      break;
+
+    case (quantidadeXP >= 2001 && quantidadeXP <= 5000):
+      console.log (resultado + "Prata!");
+      break;
+       
+    case (quantidadeXP >= 5001 && quantidadeXP <= 7000):
+      console.log (resultado + "Ouro!");
+      break;
+
+    case (quantidadeXP >= 7001 && quantidadeXP <= 8000):
+      console.log (resultado + "Platina!");
+      break;
+        
+    case (quantidadeXP >= 8001 && quantidadeXP <= 9000):
+      console.log (resultado + "Ascendente!");
+      break;
+
+    case (quantidadeXP >= 9001 && quantidadeXP <= 10000):
+      console.log (resultado + "Imortal!");
+      break;
+
+    default: 
+      console.log (resultado + "Radiante!");
+    }
+
+  console.log(" ");
+  console.log("----");
+  console.log("");
+}
+
+
+
